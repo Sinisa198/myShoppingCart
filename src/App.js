213ff31ec9ed157302d426a2eaced51a854e982d
DejanPudja <sinisaljubanovic76@gmail.com>
2022-05-17 10:52:53 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from '../public/index.html';
+
+function App() {
+  return (
+  <form>
+    <input type='text' />
+  </form>
+  );
+}
+
+export default App;
