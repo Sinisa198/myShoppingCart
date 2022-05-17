dd014446dd83a849dc4dc85f17036b35e466a71e
SinisaLjubanovic <sinisaljubanovic76@gmail.com>
2022-05-17 11:33:30 +0200

    git commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import CartItem from './cart/CartItem';
+
 
 function App() {
   return (
-    <form>
-      <div>
-        <h1 className="title">Hello to my shopping cart</h1>
-        <CartItem />
-      </div>
-    </form>
+    <div>
+  
+    </div>
   );
 }
 
